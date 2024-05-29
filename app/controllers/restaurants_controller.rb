9157fd1d21a073c82fdf84b5a2a2988013abc2c0
Leo Casas <digid@digid.com.br>
2024-05-29 12:35:32 -0300

    Test connection

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -18,17 +18,27 @@ class RestaurantsController < ApplicationController
     redirect_to restaurant_path(@restaurant)
   end
 
+  def edit
+  end
+
   def update
-    @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
+    @restaurant.update(restaurant_params)
     redirect_to restaurant_path(@restaurant)
   end
 
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path status: :see_other
+  end
 
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :stars)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'restaurants', to: 'restaurants#index'
   get 'restaurants/new', to: 'restaurants#new'
   get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
   post 'restaurants', to: 'restaurants#create'
-  get 'restaurants/:id/edit', to: 'restaurants#edit'
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
   patch 'restaurants/:id', to: 'restaurants#update'
+  delete 'restaurants/:id', to: 'restaurants#destroy'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
