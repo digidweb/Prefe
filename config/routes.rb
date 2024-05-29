45498856ec858345fefa71e0aa1a4dd44eec55ce
Leo Casas <digid@digid.com.br>
2024-05-29 02:21:38 -0300

    Models, Controller and ROutes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id/edit', to: 'restaurants#edit'
+  patch 'restaurants/:id', to: 'restaurants#update'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
