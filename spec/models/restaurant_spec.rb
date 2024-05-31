ffb1504dd58c8a9612c909063b502ca3ecc89735
Leo Casas <digid@digid.com.br>
2024-05-31 20:56:53 -0300

    Add RSpec and initial configuration

---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Restaurant, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'initial validattion' do
+    expect(1).to eq(1)
+  end
+
+  it 'initial validattion' do
+    expect(1).to eq(2)
+  end
+
+end
